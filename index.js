4d4790c8c059b9f7940739b57e346347ce17d628
philipmo <philipmo59@gmail.com>
2023-04-26 12:17:27 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(userName,userEmail){
+    return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        "name": userName,
+        "email": userEmail,        
+    })
+})
+.then(res => res.json())
+// .then(document.getElementById("1").appendChild(response))
+.then(response=> document.body.append(response.id))
+.catch(error=> document.body.append(error.message))
+}
+
+submitData("philip","philipmo59@gmail.com")
